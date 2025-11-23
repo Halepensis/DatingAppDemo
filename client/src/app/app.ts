4f3da47103748ca141444343cd92d96774d620cd
Halepensis <antgranada@gmail.com>
2025-11-23 19:19:02 +0100

    feat: enhance app configuration and UI
    - Added HttpClient provider to app configuration for API calls.
    - Updated app HTML to display a list of members with avatars.
    - Modified app component to fetch members from the API and display them.
    - Integrated Tailwind CSS and DaisyUI for styling.
    - Added SSL certificates for local development.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal, Signal, WritableSignal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('client');
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members: Signal<any[]> = toSignal(
+    this.http.get<any[]>('https://localhost:5001/api/members'),
+    {
+      initialValue: [],
+    }
+  );
 }
