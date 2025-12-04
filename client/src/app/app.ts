d99f99a44a1b8cd9179c93fe553bb89b17bc9afd
Halepensis <antgranada@gmail.com>
2025-12-04 22:20:59 +0100

    feat: implement user registration and login functionality with navigation

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,20 +3,35 @@ import { Component, inject, OnInit, signal, Signal, WritableSignal } from '@angu
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from '../layout/nav/nav';
+import { User } from '../types/user';
+import { AccountService } from '../core/services/account-service';
+import { Home } from '../features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
-  protected members: Signal<any[]> = toSignal(
-    this.http.get<any[]>('https://localhost:5001/api/members'),
+  protected members: Signal<User[]> = toSignal(
+    this.http.get<User[]>('https://localhost:5001/api/members'),
     {
       initialValue: [],
     }
   );
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
 }
