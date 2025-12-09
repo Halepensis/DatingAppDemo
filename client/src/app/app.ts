fbdddefe9b4eb4dc5d3ce1e52cd1b4470f647174
Halepensis <antgranada@gmail.com>
2025-12-09 22:47:54 +0100

    feat: implement routing, guards, and initialization services; enhance navigation and UI components

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,22 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal, Signal, WritableSignal } from '@angular/core';
+import { Component, inject, signal, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from '../layout/nav/nav';
 import { User } from '../types/user';
-import { AccountService } from '../core/services/account-service';
-import { Home } from '../features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
+export class App {
   private http = inject(HttpClient);
+  protected router = inject(Router);
   protected readonly title = signal('Dating App');
   protected members: Signal<User[]> = toSignal(
     this.http.get<User[]>('https://localhost:5001/api/members'),
@@ -24,14 +21,4 @@ export class App implements OnInit {
       initialValue: [],
     }
   );
-  ngOnInit(): void {
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,28 +1,42 @@
 import { Component, inject, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginCreds } from '../../types/user';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
-  protected creds = {
+  private toastService = inject(ToastService);
+  private router = inject(Router);
+
+  protected creds: LoginCreds = {
     email: '',
     password: '',
   };
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (res) => {},
-      error: (err) => console.error(err.message),
+      next: (res) => {
+        this.router.navigateByUrl('/members');
+        console.log(res);
+        this.toastService.success('Logged in successfully');
+      },
+      error: (err) => {
+        console.log(err);
+        this.toastService.error(err.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
